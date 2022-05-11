2340f29ca3106341e8cd4e3b91e811390ab73197
pgehring <philipp_gehring@yahoo.de>
2022-05-11 17:51:39 +0200

    apply clang-format

--- project/parallelFEM/Source/hpc_prol.c
+++ project/parallelFEM/Source/hpc_prol.c
@@ -1,14 +1,12 @@
 #include "hpc.h"
 /* cg-olver with constrains */
-void hpc_prol(double *x, index nx, index *edgeno, index nEdges, double *y)
-{ 
+void hpc_prol(double *x, index nx, index *edgeno, index nEdges, double *y) {
   index i;
 
-  for (i = 0; i < nx; i++) y[i] += x[i];  
-  for (i = 0; i < nEdges; i++){
-    y[nx+i] += 0.5 * (x[edgeno[2*i  ]]
-                    + x[edgeno[2*i+1]]);
+  for (i = 0; i < nx; i++)
+    y[i] += x[i];
+  for (i = 0; i < nEdges; i++) {
+    y[nx + i] += 0.5 * (x[edgeno[2 * i]] + x[edgeno[2 * i + 1]]);
   }
   return;
 }
-                                 
